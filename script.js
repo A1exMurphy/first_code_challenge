df19b8b1ed81ffd0e90730f90a18aa936d2ffa3c
Evan Langer <elanger@live.com>
2023-10-25 15:31:43 -0500

    updates

--- script.js
+++ script.js
@@ -15,16 +15,16 @@ console.log(myScrabbleTiles);
   { tile: 'D', score: 2 }
   { tile: 'A', score: 1 }
 */
-let tile14 = {letter: "N", score: 1};
-let tile11 = {letter: "K", score: 5};
-let tile26 = {letter: "Z", score: 10};
-let tile24 = {letter: "X", score: 8};
-let tile4 = {letter: "D", score: 2};
-let tile1 = {letter: "A", score: 1};
-let tile6 = {letter: "F", score: 4};
+let tile14 = {tile: "N", score: 1};
+let tile11 = {tile: "K", score: 5};
+let tile26 = {tile: "Z", score: 10};
+let tile24 = {tile: "X", score: 8};
+let tile4 = {tile: "D", score: 2};
+let tile1 = {tile: "A", score: 1};
+let tile6 = {tile: "F", score: 4};
 
-myScrabbleTiles.push("draw six tiles:", tile14, tile11, tile26, tile24, tile4, tile1);
-console.log(myScrabbleTiles);
+myScrabbleTiles.push( tile14, tile11, tile26, tile24, tile4, tile1);
+console.log("draw six tiles:",myScrabbleTiles);
 
 // 4. Remove the last tile from myScrabbleTiles and save it in a variable named removedTile.
 let removedTile = myScrabbleTiles.pop();
@@ -44,17 +44,17 @@ console.log(myScrabbleTiles);
 let myScore = 0
 
 function calculateScore(myScrabbleTiles) {
-  myScrabbleTiles.forEach((element)=> {
+  myScrabbleTiles.forEach((myScrabbleTiles.score)=> {
   console.log(element.score)
   });
   myScore += myScrabbleTiles(element.score);}
-calculateScore
-console.log(calculateScore);  //I feel like I'm so close to getting this to work but I'm not certain if you can add with the forEach operator
+// calculateScore
+            // console.log(calculateScore);  //I feel like I'm so close to getting this to work but I'm not certain if you can add with the forEach operator
 
-// 7. Use the function above to get the total score for myScrabbleTiles and assign the value to a variable called "myScore".
-console.log(calculateScore(){
- return myScore = calculateScore;
-});
+            // // 7. Use the function above to get the total score for myScrabbleTiles and assign the value to a variable called "myScore".
+            // console.log(calculateScore(){
+            //  return myScore = calculateScore;
+            // });
 
 // 8. Check whether or not your score is higher than the highScore. If your score is higher, change highScore to the new high score.
 if (myScore > highScore) {
